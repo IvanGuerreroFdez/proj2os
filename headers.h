2a0216740a502a2a1a51c76e0b0ea202cb9a1ec5
Hackkill <theiviboss083@gmail.com>
2023-12-15 12:54:58 +0100

    2 functions implemented

--- headers.h
+++ headers.h
@@ -3,51 +3,51 @@
 #define MAX_FICHEROS 20
 #define MAX_BLOQUES_DATOS 96
 #define PRIM_BLOQUE_DATOS 4
-#define MAX_BLOQUES_PARTICION MAX_BLOQUES_DATOS+PRIM_BLOQUE_DATOS
-  //superbloque + bytemap inodos y bytemap bloques + inodos + directorio
+#define MAX_BLOQUES_PARTICION MAX_BLOQUES_DATOS+PRIM_BLOQUE_DATOS // 96 + 4 = 100
+  //Superblock + Bytemap Inode & Bytemap Block + Inodes + Directory
 #define MAX_NUMS_BLOQUE_INODO 7
 #define LEN_NFICH 17
 #define NULL_INODO 0xFFFF
 #define NULL_BLOQUE 0xFFFF
 
-/* Estructura del superbloque */
+/* Superblock structure */
 typedef struct {
-  unsigned int s_inodes_count;          /* inodos de la partición */
-  unsigned int s_blocks_count;          /* bloques de la partición */
-  unsigned int s_free_blocks_count;     /* bloques libres */
-  unsigned int s_free_inodes_count;     /* inodos libres */
-  unsigned int s_first_data_block;      /* primer bloque de datos */
-  unsigned int s_block_size;        /* tamaño del bloque en bytes */
-  unsigned char s_relleno[SIZE_BLOQUE-6*sizeof(unsigned int)]; /* relleno a 0's*/
+  unsigned int s_inodes_count;          /* Partitition inodes */
+  unsigned int s_blocks_count;          /* Partitition blocks */
+  unsigned int s_free_blocks_count;     /* Free blocks */
+  unsigned int s_free_inodes_count;     /* Free inodes */
+  unsigned int s_first_data_block;      /* First data block */
+  unsigned int s_block_size;        /* Block size in bytes */
+  unsigned char s_relleno[SIZE_BLOQUE-6*sizeof(unsigned int)]; /* filling to 0's*/
 } EXT_SIMPLE_SUPERBLOCK;
 
-/* Bytemaps, caben en un bloque */
+/* Bytemaps in each block */
 typedef struct {
   unsigned char bmap_bloques[MAX_BLOQUES_PARTICION];
-  unsigned char bmap_inodos[MAX_INODOS];  /* inodos 0 y 1 reservados, inodo 2 directorio */
+  unsigned char bmap_inodos[MAX_INODOS];  /* inode 0 & 1 reserved, inode 2 directory */
   unsigned char bmap_relleno[SIZE_BLOQUE-(MAX_BLOQUES_PARTICION+MAX_INODOS)*sizeof(char)];
 } EXT_BYTE_MAPS;
 
-/* inodo */
+/* inode */
 typedef struct {
   unsigned int size_fichero;
   unsigned short int i_nbloque[MAX_NUMS_BLOQUE_INODO];
 } EXT_SIMPLE_INODE;
 
-/* Lista de inodos, caben en un bloque */
+/* List of inodes that fit on a block */
 typedef struct {
   EXT_SIMPLE_INODE blq_inodos[MAX_INODOS];
   unsigned char blq_relleno[SIZE_BLOQUE-MAX_INODOS*sizeof(EXT_SIMPLE_INODE)];
 } EXT_BLQ_INODOS;
 
-/* Entrada individual del directorio */
+/* Individual entry of the directory */
 typedef struct {
   char dir_nfich[LEN_NFICH];
   unsigned short int dir_inodo;
   //Agregar nuevo parametro para permisos
 } EXT_ENTRADA_DIR;
 
-/* Bloque de datos */
+/* Data blocks */
 typedef struct{
   unsigned char dato[SIZE_BLOQUE]; 	
 } EXT_DATOS;
--- simul_ext.c
+++ simul_ext.c
@@ -1,21 +1,111 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
+#include <dirent.h> // Used to see files in a directory
+#include <ctype.h>
+#include <stdbool.h>
 #include "headers.h"
 
-#define COMLEN 100 // Command lengthss
+#define COMLEN 100 // Command length
+#define SIZE_BLOQUE 512
+#define MAX_INODOS 24
+#define MAX_FICHEROS 20
+#define MAX_BLOQUES_DATOS 96
+#define PRIM_BLOQUE_DATOS 4
+#define MAX_BLOQUES_PARTICION MAX_BLOQUES_DATOS+PRIM_BLOQUE_DATOS // 96 + 4 = 100
+#define MAX_NUMS_BLOQUE_INODO 7
+#define LEN_NFICH 17
+#define NULL_INODO 0xFFFF
+#define NULL_BLOQUE 0xFFFF
 
-void printByteMaps(EXT_BYTE_MAPS *ext_bytemaps) {} // end of printByteMaps
+void printByteMaps(EXT_BYTE_MAPS *ext_bytemaps) {
+    // Prints inodes
+    printf("Inodes: ");
+    for(int i = 0; i < MAX_INODOS; i++) {
+        printf("%d ", ext_bytemaps->bmap_inodos[i]);
+    } // end for loop
 
-int checkCommand(char *strcommand, char *order, char *argument1, char *argument2) {} // end of checkCommand
+    // Prints first 25 blocks
+    printf("\nBlocks [0 - 25]: ");
+    for(int i = 0; i < 25; i++) {
+        printf("%d ", ext_bytemaps->bmap_bloques[i]);
+    } // end for loop
+    printf("\n");
+} // end of printByteMaps
+
+int checkCommand(char *strcommand, char *order, char *argument1, char *argument2) {
+    int count = sscanf(strcommand, "%s %s %s", order, argument1, argument2);
+
+    if(count==1){
+        if(strcmp(order, "info")==0 || strcmp(order, "bytemaps")==0 || strcmp(order, "dir")==0 || strcmp(order, "exit")==0) {
+            return 0;
+        } // end if condition
+    } else if (count==2){
+        if(strcmp(order, "rename")==0 || strcmp(order, "imprimir")==0 || strcmp(order, "remove")==0) {
+            return 0;
+        }
+    } else if (count==3){
+        if(strcmp(order, "copy")==0) {
+            return 0;
+        } // end if condition
+    } // end if, else if, else if conditions
+
+    printf("Error in the command. Try again!\n");
+    return -1;
+} // end of checkCommand
 
 void readSuperBlock(EXT_SIMPLE_SUPERBLOCK *psup) {} // end of readSuperBlock
 
 int searchFile(EXT_ENTRADA_DIR *directory, EXT_BLQ_INODOS *inodes, char *name) {} // end of searchFile
 
-void directory(EXT_ENTRADA_DIR *directory, EXT_BLQ_INODOS *inodes) {} // end of directory
+void directory(EXT_ENTRADA_DIR *directory, EXT_BLQ_INODOS *inodes) {
+    DIR *dr = opendir("."); // Opens current directory
+    struct dirent *en;
 
-int renameFile(EXT_ENTRADA_DIR *directory, EXT_BLQ_INODOS *inodes, char *nombreantiguo, char *nombrenuevo) {} // end of renameFile
+    if(dr) { // If the directory exists
+        while((en = readdir(dr)) != NULL) { // goes over files in the directory
+            printf("%c\t", directory->dir_nfich);
+            printf("Size: %d\t", inodes->blq_inodos->size_fichero);
+            printf("Inode: %d\t", directory->dir_inodo);
+            printf("Blocks: ");
+            for(int i = 0; i < sizeof(inodes->blq_relleno); i++) { // Not sure about this
+                printf("%d ", inodes->blq_relleno[i]); // Not sure about this
+            } // end for loop
+        } // end while loop
+    } // end if condition
+    
+    closedir(dr);
+} // end of directory
+
+int renameFile(EXT_ENTRADA_DIR *directory, EXT_BLQ_INODOS *inodes, char *nombreantiguo, char *nombrenuevo) {
+    DIR *dr = opendir(".");
+    struct dirent *en;
+    bool srcFileEx = false;
+    bool newNameFileEx = false;
+
+    if(dr) {
+        while((en = readdir(dr)) != NULL) {
+            if(directory->dir_nfich == nombreantiguo) { // Check if source file exists
+                srcFileEx = true;
+            } else if(directory->dir_nfich == nombrenuevo) { // Checks if file with the new name exists
+                newNameFileEx = true;
+            } // end if, else if conditions
+        } // end while loop
+    } // end if condition
+    
+    if(srcFileEx == false) { // If source file does not exist
+        printf("ERROR: File %c is not found\n", nombreantiguo);
+    } else if(newNameFileEx) { // If file with the new name exists
+        printf("ERROR: File %c already exists\n", nombrenuevo);
+    } else { // If everything is correct --> no error
+        int ren = rename(nombreantiguo, nombrenuevo); // Library already included in C
+
+        if(ren = -1) {
+            printf("ERROR: Unable to rename the file\n");
+        } // end if condition
+    } // end if, else if, else conditions
+} // end of renameFile
 
 int print(EXT_ENTRADA_DIR *directory, EXT_BLQ_INODOS *inodes, EXT_DATOS *memdata, char *nombre) {} // end of print
 
@@ -33,7 +123,9 @@ void recordByteMaps(EXT_BYTE_MAPS *ext_bytemaps, FILE *f) {} // end of recordByt
 
 void recordSuperBlock(EXT_SIMPLE_SUPERBLOCK *ext_superblock, FILE *f) {} // end of recordSuperBlock
 
-void recordData(EXT_DATOS *memdata, FILE *f) {} // end of recordData
+void recordData(EXT_DATOS *memdata, FILE *f) {
+    
+} // end of recordData
 
 int main() {
     char *command[COMLEN];
@@ -50,9 +142,8 @@ int main() {
     EXT_DATOS fileData[MAX_BLOQUES_PARTICION];
     int dirEntrance;
     int recordData;
-    FILE *entranceFile;
+    FILE *entranceFile = fopen("partition.bin", "r+b");
 
-    entranceFile = fopen("partition.bin", "r+b");
     fread(&fileData, SIZE_BLOQUE, MAX_BLOQUES_PARTICION, entranceFile); 
 
     memcpy(&ext_superblock,(EXT_SIMPLE_SUPERBLOCK *)&fileData[0], SIZE_BLOQUE);
@@ -68,7 +159,7 @@ int main() {
             fgets(command, COMLEN, stdin);
         } while(checkCommand(command, order, argument1, argument2) != 0);
 
-        if(strcmp(order, "dir")) {
+        if (strcmp(order, "dir") == 0) {
             directory(&directory, &ext_blq_inodes);
             continue;
         } // end if condition
@@ -77,7 +168,7 @@ int main() {
         recordByteMaps(&ext_bytemaps, entranceFile);
         recordSuperBlock(&ext_superblock, entranceFile);
 
-        if(recordData) {
+        if (recordData) {
             recordData(&memData, entranceFile);
         } // end if condition
 
