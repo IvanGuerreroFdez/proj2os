a64d6b3a6b71e94ba6533ea3d169242ce3002ef1
Hackkill <theiviboss083@gmail.com>
2023-12-18 16:19:33 +0100

    Code improvement. Several code erros fixed. Huge error: 'particion.bin' is not been readed correctly

--- simul_ext.c
+++ simul_ext.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/types.h>
-#include <dirent.h> // Used to see files in a directory
-#include <ctype.h>
 #include <stdbool.h>
 #include "headers.h"
 
-#define COMLEN 100 // Command length
+#define COMLEN 100 // Command len
 #define SIZE_BLOQUE 512
 #define MAX_INODOS 24
 #define MAX_FICHEROS 20
@@ -19,235 +16,272 @@
 #define NULL_INODO 0xFFFF
 #define NULL_BLOQUE 0xFFFF
 
-// Function to print bytemaps
 void printByteMaps(EXT_BYTE_MAPS *ext_bytemaps) {
-    // Prints inodes
     printf("Inodes: ");
-    for(int i = 0; i < MAX_INODOS; i++) {
+    for (int i = 0; i < MAX_INODOS; i++) {
         printf("%d ", ext_bytemaps->bmap_inodos[i]);
-    } // end for loop
+    }
 
-    // Prints first 25 blocks
     printf("\nBlocks [0 - 25]: ");
-    for(int i = 0; i < 25; i++) {
+    for (int i = 0; i < 25; i++) {
         printf("%d ", ext_bytemaps->bmap_bloques[i]);
-    } // end for loop
+    }
     printf("\n");
-} // end of printByteMaps
+}
 
-// Function to check if a command has the correct arguments
 int checkCommand(char *strcommand, char *order, char *argument1, char *argument2) {
     int count = sscanf(strcommand, "%s %s %s", order, argument1, argument2);
 
-    if(count==1){
-        if(strcmp(order, "info")==0 || strcmp(order, "bytemaps")==0 || strcmp(order, "dir")==0 || strcmp(order, "exit")==0) {
-            return 0;
-        } // end if condition
-    } else if (count==2){
-        if(strcmp(order, "rename")==0 || strcmp(order, "print")==0 || strcmp(order, "remove")==0) {
+    if (count == 1) {
+        if (strcmp(order, "info") == 0 || strcmp(order, "bytemaps") == 0 || strcmp(order, "dir") == 0 || strcmp(order, "exit") == 0) {
             return 0;
         }
-    } else if (count==3){
-        if(strcmp(order, "copy")==0) {
+    } else if (count == 2) {
+        if (strcmp(order, "rename") == 0 || strcmp(order, "print") == 0 || strcmp(order, "remove") == 0) {
             return 0;
-        } // end if condition
-    } // end if, else if, else if conditions
-
-    printf("Error in the command. Try again!\n");
+        }
+    } else if (count == 3) {
+        if (strcmp(order, "copy") == 0) {
+            return 0;
+        }
+    }
+    printf("Invalid Command. Try again please\n");
     return -1;
-} // end of checkCommand
+}
 
-// Function to read superblock
 void readSuperBlock(EXT_SIMPLE_SUPERBLOCK *psup) {
     printf("Inodes count = %u\n", psup->s_inodes_count);
     printf("Blocks count = %u\n", psup->s_blocks_count);
     printf("Free Inodes count = %u\n", psup->s_free_blocks_count);
     printf("Free Blocks count = %u\n", psup->s_free_inodes_count);
     printf("First data block = %u\n", psup->s_first_data_block);
-    printf("Block Size: %u\n bytes", psup->s_block_size);
-} // end of readSuperBlock
+    printf("Block Size: %u bytes\n", psup->s_block_size);
+}
 
-// Function to find if a file exists
 int searchFile(EXT_ENTRADA_DIR *directory, EXT_BLQ_INODOS *inodes, char *name) {
-    while() {
-        if(directory->dir_nfich == name) {
-            return 0; // Returns 0 if the file is found
-        } else {
-            return -1; // Returns -1 if the file is NOT found
-        } // end if else conditions
-    } // end while loop
-} // end of searchFile
+    for (int i = 0; i < MAX_FICHEROS; i++) {
+        if (strcmp(directory[i].dir_nfich, name) == 0) {
+            return i;
+        }
+    }
+    return -1;
+}
 
-// Function to show the contents of a directory
 void dir(EXT_ENTRADA_DIR *directory, EXT_BLQ_INODOS *inodes) {
-    DIR *dr = opendir("."); // Opens directory
-    struct dirent *en;
-
-    if(dr) { // If the directory exists
-        while((en = readdir(dr)) != NULL) { // goes over files in the directory
-            printf("%c\t", directory->dir_nfich);
-            printf("Size: %d\t", inodes->blq_inodos->size_fichero);
-            printf("Inode: %d\t", directory->dir_inodo);
+    for (int i = 0; i < MAX_FICHEROS; i++) {
+        if (directory[i].dir_inodo != NULL_INODO) {
+            printf("%s\t", directory[i].dir_nfich);
+            printf("Size: %d\t", inodes->blq_inodos[directory[i].dir_inodo].size_fichero);
+            printf("Inode: %d\t", directory[i].dir_inodo);
             printf("Blocks: ");
-            for(int i = 0; i < directory->dir_inodo; i++) { // Not sure about this
-                printf("%d ", inodes->blq_relleno[i]); // Not sure about this
-            } // end for loop
+            for (int j = 0; j < MAX_NUMS_BLOQUE_INODO; j++) {
+                printf("%d ", inodes->blq_inodos[directory[i].dir_inodo].i_nbloque[j]);
+            }
             printf("\n");
-        } // end while loop
-    } // end if condition
-    
-    closedir(dr);
-} // end of directory
+        }
+    }
+}
 
-// Function to rename a file
-int renameFile(EXT_ENTRADA_DIR *directory, EXT_BLQ_INODOS *inodes, char *nombreantiguo, char *nombrenuevo) {
-    DIR *dr = opendir("particion.bin");
-    struct dirent *en;
-    bool srcFileEx = false;
-    bool newNameFileEx = false;
-
-    if(dr) {
-        while((en = readdir(dr)) != NULL) {
-            if(directory->dir_nfich == nombreantiguo) { // Check if source file exists
-                srcFileEx = true;
-            } else if(directory->dir_nfich == nombrenuevo) { // Checks if file with the new name exists
-                newNameFileEx = true;
-            } // end if, else if conditions
-        } // end while loop
-    } // end if condition
-    
-    if(srcFileEx == false) { // If source file does not exist
-        printf("ERROR: File %c is not found\n", nombreantiguo);
-    } else if(newNameFileEx) { // If file with the new name exists
-        printf("ERROR: File %c already exists\n", nombrenuevo);
-    } else { // If everything is correct --> no error
-        int ren = rename(nombreantiguo, nombrenuevo); // Library already included in C
-
-        if(ren = -1) {
-            printf("ERROR: Unable to rename the file\n");
-        } // end if condition
-    } // end if, else if, else conditions
-} // end of renameFile
-
-// Prints the contents of a file
-int print(EXT_ENTRADA_DIR *directory, EXT_BLQ_INODOS *inodes, EXT_DATOS *memdata, char *nombre) {
-    FILE *f = fopen(nombre, "r"); // Opens the file with the name "nombre"
-    char c;
-
-    if(f == NULL) {
-        printf("ERROR: File not found\n");
+int renameFile(EXT_ENTRADA_DIR *directory, char *nombreantiguo, char *nombrenuevo) {
+    int fileIndex = searchFile(directory, NULL, nombreantiguo);
+    if (fileIndex != -1) {
+        if (searchFile(directory, NULL, nombrenuevo) == -1) {
+            strcpy(directory[fileIndex].dir_nfich, nombrenuevo);
+            printf("File %s succesfully renamed!.\n", nombreantiguo);
+        } else {
+            printf("ERROR: the file already exist (remember is case sensitive and try again).\n");
+        }
     } else {
-        while((c = getc(f)) != EOF) {
-            printf("%c", c);
-        } // end while loop
-    } // end if else
+        printf("ERROR: file %s is not found. Use 'dir' to check the files ^^\n", nombreantiguo);
+    }
+    return 0;
+}
 
+int print(char *nombre) {
+    FILE *f = fopen(nombre, "r");
+    if (f == NULL) {
+        printf("ERROR: file not found. Use 'dir' to check the files ^^\n");
+        return -1;
+    }
+    char c;
+    while ((c = getc(f)) != EOF) {
+        printf("%c", c);
+    }
     fclose(f);
-} // end of print
+    return 0;
+}
 
-// Function to delete a file
-int delete(EXT_ENTRADA_DIR *directory, EXT_BLQ_INODOS *inodes,
-           EXT_BYTE_MAPS *ext_bytemaps, EXT_SIMPLE_SUPERBLOCK *ext_superblock,
-           char *name,  FILE *f) {} // end of delete
+int delete(EXT_ENTRADA_DIR *directory, EXT_BLQ_INODOS *inodes, EXT_BYTE_MAPS *ext_bytemaps, char *name) {
+    int fileIndex = searchFile(directory, inodes, name);
 
-// Function to copy a file
-int copy(EXT_ENTRADA_DIR *directory, EXT_BLQ_INODOS *inodes,
-           EXT_BYTE_MAPS *ext_bytemaps, EXT_SIMPLE_SUPERBLOCK *ext_superblock,
-           EXT_DATOS *memdata, char *originName, char *destName,  FILE *f) {} // end of copy
+    if (fileIndex != -1) {
+        int inodeIndex = directory[fileIndex].dir_inodo;
+        for (int i = 0; i < MAX_NUMS_BLOQUE_INODO && inodes->blq_inodos[inodeIndex].i_nbloque[i] != NULL_BLOQUE; i++) {
+            ext_bytemaps->bmap_bloques[inodes->blq_inodos[inodeIndex].i_nbloque[i]] = 0;
+        }
+        ext_bytemaps->bmap_inodos[inodeIndex] = 0;
+        memset(&directory[fileIndex], 0, sizeof(EXT_ENTRADA_DIR));
+        printf("The file %s has been deleted succesfully :)\n", name);
+    } else {
+        printf("ERROR: file %s is not found. Use 'dir' to check the files ^^\n", name);
+    }
+    return 0;
+}
 
-// Function to
-void recordInodeAndDirectory(EXT_ENTRADA_DIR *directory, EXT_BLQ_INODOS *inodes, FILE *f) {} // end of recordInodeDirectory
+int copy(EXT_ENTRADA_DIR *directory, EXT_BLQ_INODOS *inodes, EXT_BYTE_MAPS *ext_bytemaps,
+         EXT_SIMPLE_SUPERBLOCK *ext_superblock, char *originName, char *destName) {
+    int srcIndex = searchFile(directory, inodes, originName);
+    int destIndex = searchFile(directory, NULL, destName);
+    if (srcIndex != -1) {
+        if (destIndex == -1) {
+            int srcInodeIndex = directory[srcIndex].dir_inodo;
+            int destInodeIndex = -1;
+            for (int i = 0; i < MAX_INODOS; i++) {
+                if (ext_bytemaps->bmap_inodos[i] == 0) {
+                    destInodeIndex = i;
+                    break;
+                }
+            }
 
-// Function to save bytemaps
-void recordByteMaps(EXT_BYTE_MAPS *ext_bytemaps, FILE *f) {} // end of recordByteMaps
+            if (destInodeIndex != -1) {
+                int destBlockIndices[MAX_NUMS_BLOQUE_INODO];
+                int numBlocks = 0;
+                for (int i = 0; i < MAX_NUMS_BLOQUE_INODO; i++) {
+                    if (ext_bytemaps->bmap_bloques[i] == 0) {
+                        destBlockIndices[numBlocks++] = i;
+                    }
 
-// Function to save superblock 
-void recordSuperBlock(EXT_SIMPLE_SUPERBLOCK *ext_superblock, FILE *f) {} // end of recordSuperBlock
+                    if (numBlocks == inodes->blq_inodos[srcInodeIndex].size_fichero) {
+                        break;
+                    }
+                }
 
-// Function to save data
-void recordData(EXT_DATOS *memdata, FILE *f) {} // end of recordData
+                if (numBlocks == inodes->blq_inodos[srcInodeIndex].size_fichero) {
+                    strcpy(directory[srcIndex].dir_nfich, destName);
+                    directory[srcIndex].dir_inodo = destInodeIndex;
+                    memcpy(&inodes->blq_inodos[destInodeIndex], &inodes->blq_inodos[srcInodeIndex], SIZE_BLOQUE);
+                    ext_bytemaps->bmap_inodos[destInodeIndex] = 1;
+                    for (int i = 0; i < numBlocks; i++) {
+                        memmove(&ext_superblock->s_first_data_block + destBlockIndices[i],
+                                &ext_superblock->s_first_data_block + inodes->blq_inodos[srcInodeIndex].i_nbloque[i], SIZE_BLOQUE);
+                        ext_bytemaps->bmap_bloques[destBlockIndices[i]] = 1;
+                    }
+                    printf("The file %s has been succesfully copied into %s.\n", originName, destName);
+                } else {
+                    printf("ERROR: Not enough free blocks\n");
+                }
+            } else {
+                printf("ERROR: Not enough inodes\n");
+            }
+        } else {
+            printf("ERROR: destination file %s already exist (remember is case sensitive and try again).\n", destName);
+        }
+    } else {
+        printf("ERROR: file %s is not found. Use 'dir' to check the files ^^\n", originName);
+    }
+
+    return 0;
+}
+
+void recordInodeAndDirectory(EXT_ENTRADA_DIR *directory, EXT_BLQ_INODOS *inodes, FILE *f) {
+    fseek(f, SIZE_BLOQUE * 3, SEEK_SET);
+    fwrite(directory, SIZE_BLOQUE, 1, f);
+    fwrite(inodes, SIZE_BLOQUE, 1, f);
+}
+
+void recordByteMaps(EXT_BYTE_MAPS *ext_bytemaps, FILE *f) {
+    fseek(f, SIZE_BLOQUE, SEEK_SET);
+    fwrite(ext_bytemaps, SIZE_BLOQUE, 1, f);
+}
+
+void recordSuperBlock(EXT_SIMPLE_SUPERBLOCK *ext_superblock, FILE *f) {
+    fseek(f, 0, SEEK_SET);
+    fwrite(ext_superblock, SIZE_BLOQUE, 1, f);
+}
+
+void recordDataBlocks(EXT_DATOS *memdata, FILE *f) {
+    fseek(f, SIZE_BLOQUE * 4, SEEK_SET);
+    fwrite(memdata, SIZE_BLOQUE, MAX_BLOQUES_DATOS, f);
+}
 
 int main() {
-    char *command = (char *) malloc(sizeof(char) * COMLEN);
-    char *order = (char *) malloc(sizeof(char) * COMLEN);
-	char *argument1 = (char *) malloc(sizeof(char) * COMLEN);
-	char *argument2 = (char *) malloc(sizeof(char) * COMLEN);
+    char *command = (char *)malloc(sizeof(char) * COMLEN);
+    char *order = (char *)malloc(sizeof(char) * COMLEN);
+    char *argument1 = (char *)malloc(sizeof(char) * COMLEN);
+    char *argument2 = (char *)malloc(sizeof(char) * COMLEN);
 
-    unsigned long int m;
     EXT_SIMPLE_SUPERBLOCK ext_superblock;
     EXT_BYTE_MAPS ext_bytemaps;
     EXT_BLQ_INODOS ext_blq_inodes;
     EXT_ENTRADA_DIR directory[MAX_FICHEROS];
     EXT_DATOS memData[MAX_BLOQUES_DATOS];
     EXT_DATOS fileData[MAX_BLOQUES_PARTICION];
-    int dirEntrance;
-    int recordData;
-    FILE *entranceFile = fopen("partition.bin", "r+b");
+    int recordData = 0;
 
-    fread(&fileData, SIZE_BLOQUE, MAX_BLOQUES_PARTICION, entranceFile); 
+    FILE *entranceFile = fopen("partition.bin", "rb");
+    fread(&fileData, SIZE_BLOQUE, MAX_BLOQUES_PARTICION, entranceFile);
 
-    memcpy(&ext_superblock,(EXT_SIMPLE_SUPERBLOCK *)&fileData[0], SIZE_BLOQUE);
-    memcpy(&directory,(EXT_ENTRADA_DIR *)&fileData[3], SIZE_BLOQUE);
-    memcpy(&ext_bytemaps,(EXT_BLQ_INODOS *)&fileData[1], SIZE_BLOQUE);
-    memcpy(&ext_blq_inodes,(EXT_BLQ_INODOS *)&fileData[2], SIZE_BLOQUE);
-    memcpy(&memData,(EXT_DATOS *)&fileData[4],MAX_BLOQUES_DATOS*SIZE_BLOQUE);
+    memcpy(&ext_superblock, (EXT_SIMPLE_SUPERBLOCK *)&fileData[0], SIZE_BLOQUE);
+    memcpy(&directory, (EXT_ENTRADA_DIR *)&fileData[3], SIZE_BLOQUE);
+    memcpy(&ext_bytemaps, (EXT_BYTE_MAPS *)&fileData[1], SIZE_BLOQUE);
+    memcpy(&ext_blq_inodes, (EXT_BLQ_INODOS *)&fileData[2], SIZE_BLOQUE);
+    memcpy(&memData, (EXT_DATOS *)&fileData[4], MAX_BLOQUES_DATOS * SIZE_BLOQUE);
 
-    for(;;) {
+    for (;;) {
         do {
             printf(">> ");
-            fflush(stdin); // Cleans buffer for stdin
+            fflush(stdin);
             fgets(command, COMLEN, stdin);
-        } while(checkCommand(command, order, argument1, argument2) != 0);
+            command[strcspn(command, "\n")] = '\0';
+        } while (checkCommand(command, order, argument1, argument2) != 0);
 
-        // Commands to read: info, bytemaps, dir, rename, print, remove, copy, exit
+        // Commands: info, bytemaps, dir, rename, print, remove, copy, exit
         if (strcmp(order, "dir") == 0) {
             dir(directory, &ext_blq_inodes);
             continue;
-        } else if(strcmp(order, "info") == 0) {
-            
-        } else if(strcmp(order, "bytemaps")) {
+        } else if (strcmp(order, "info") == 0) {
+            readSuperBlock(&ext_superblock);
+            continue;
+        } else if (strcmp(order, "bytemaps") == 0) {
             printByteMaps(&ext_bytemaps);
-            //continue;
-        } else if(strcmp(order, "rename")) {
-            if(checkCommand("rename", order, argument1, argument2) == 0) {
-                rename(argument1, argument2);
-            } // end if condition
-
             continue;
-        } else if(strcmp(order, "print")) {
-            print(directory, &ext_blq_inodes, memData, argument1);
+        } else if (strcmp(order, "rename") == 0) {
+            if (checkCommand("rename", order, argument1, argument2) == 0) {
+                renameFile(directory, argument1, argument2);
+            }
             continue;
-        } else if(strcmp(order, "remove")) {
-            if(checkCommand("remove", order, argument1, NULL) == 0) {
-                delete(directory, &ext_blq_inodes, &ext_bytemaps, &ext_superblock, argument1, entranceFile);
-            } // end if condition
-
+        } else if (strcmp(order, "print") == 0) {
+            print(argument1);
             continue;
-        } else if(strcmp(order, "copy")) {
-            if(checkCommand("copy", order, argument1, argument2) == 0) {
-                copy(directory, &ext_blq_inodes, &ext_bytemaps, &ext_superblock, memData, argument1, argument2, entranceFile);
-            } // end if condition
-
+        } else if (strcmp(order, "remove") == 0) {
+            if (checkCommand("remove", order, argument1, NULL) == 0) {
+                delete(directory, &ext_blq_inodes, &ext_bytemaps, argument1);
+            }
             continue;
-        } // end if, else if x7 conditions
+                } else if (strcmp(order, "copy") == 0) {
+            if (checkCommand("copy", order, argument1, argument2) == 0) {
+                copy(directory, &ext_blq_inodes, &ext_bytemaps, &ext_superblock, argument1, argument2);
+            }
+            continue;
+        } else if (strcmp(order, "exit") == 0) {
+            printf("You are exiting. Thanks for the visit, see you soon <3\n");
+            break;
+        } else {
+            printf("Command not found. Try again please.\n");
+            continue;
+        }
+    }
 
-        //recordInodeDirectory(&directory, &ext_blq_inodes, entranceFile);
-        recordByteMaps(&ext_bytemaps, entranceFile);
-        recordSuperBlock(&ext_superblock, entranceFile);
+    recordSuperBlock(&ext_superblock, entranceFile);
+    recordByteMaps(&ext_bytemaps, entranceFile);
+    recordInodeAndDirectory(directory, &ext_blq_inodes, entranceFile);
+    recordDataBlocks(memData, entranceFile);
 
-        if (recordData) {
-            //recordData(&memData, entranceFile);
-        } // end if condition
+    fclose(entranceFile);
+    free(command);
+    free(order);
+    free(argument1);
+    free(argument2);
 
-        recordData = 0;
-
-        if(strcmp(order, "exit") == 0) {
-            //recordData(&memData, entranceFile);
-            fclose(entranceFile);
-
-            return 0;
-        } // end if condition
-    } // end for loop 
-    
     return 0;
-} // end of main
+}
